e6cb6fc82f2d866d6be73a8038f15f20e5e7e95c
lam-vincent <mrccniv@gmail.com>
2025-05-14 19:09:37 +0200

    Refactor: Update AndroidManifest and layout files for MapActivity integration
    - Added permissions for fine location access in AndroidManifest.xml
    - Created layout for MapActivity with a map fragment and location display
    - Removed map fragment from MainActivity layout
    - Updated MainActivity to navigate to MapActivity

--- app/src/main/java/com/example/geophoto/MainActivity.java
+++ app/src/main/java/com/example/geophoto/MainActivity.java
@@ -1,58 +1,33 @@
 package com.example.geophoto;
 
-import android.Manifest;
-
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
-    private GoogleMap map;
-    private final int FINE_PERMISSION_CODE = 1;
-    Location currentLocation;
-
-    private FusedLocationProviderClient fusedLocationClient;
-    private TextView latitude, longitude;
-
-    private boolean positionInitialeDefinie = false;
+    private String photoPath = null;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imgAffichePhoto;
+    private Button btnPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,87 +40,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             return insets;
         });
 
-        latitude = findViewById(R.id.latitude);
-        longitude = findViewById(R.id.longitude);
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
-
-
         findViewById(R.id.btn_gallery).setOnClickListener(v -> {
             // Ouvrir la galerie
+            // Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+            // startActivity(intent);
         });
         findViewById(R.id.btn_photo).setOnClickListener(v -> {
-            // Prendre une photo
+            // Ouvrir la page de prise de photo
+            // Intent intent = new Intent(MainActivity.this, PhotoActivity.class);
+            // startActivity(intent);
         });
         findViewById(R.id.btn_map).setOnClickListener(v -> {
             // Afficher la carte
+            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
         });
 
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.map = googleMap;
-
-        LatLng p = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
-
-        // Utiliser Geocoder pour obtenir l'adresse
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        String adresse = "Adresse inconnue";
-        try {
-            List<Address> addresses = geocoder.getFromLocation(p.latitude, p.longitude, 1);
-            if (addresses != null && !addresses.isEmpty()) {
-                Address address = addresses.get(0);
-                // On récupère la ligne d'adresse complète
-                adresse = address.getAddressLine(0);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        map.addMarker(new MarkerOptions().position(p).title(adresse));
-
-        if (!positionInitialeDefinie) {
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(p, 20.f));
-            positionInitialeDefinie = true;
-        }
-    }
-
-    private void getLastLocation(){
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, FINE_PERMISSION_CODE);
-            return;
-        }
-        Task<Location> task = fusedLocationClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null){
-                    currentLocation = location;
-
-                    latitude.setText(String.valueOf(location.getLatitude()));
-                    longitude.setText(String.valueOf(location.getLongitude()));
-
-                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-                    if (mapFragment != null)
-                        mapFragment.getMapAsync( MainActivity.this);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
-        if (requestCode == FINE_PERMISSION_CODE){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLastLocation();
-            } else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
--- app/src/main/java/com/example/geophoto/MapActivity.java
+++ app/src/main/java/com/example/geophoto/MapActivity.java
@@ -1,14 +1,49 @@
 package com.example.geophoto;
 
+import android.Manifest;
+
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView latitude, longitude;
+
+    private boolean positionInitialeDefinie = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +55,81 @@ public class MapActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.map = googleMap;
+
+        LatLng p = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+        // Utiliser Geocoder pour obtenir l'adresse
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        String adresse = "Adresse inconnue";
+        try {
+            List<Address> addresses = geocoder.getFromLocation(p.latitude, p.longitude, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                // On récupère la ligne d'adresse complète
+                adresse = address.getAddressLine(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        map.addMarker(new MarkerOptions().position(p).title(adresse));
+
+        if (!positionInitialeDefinie) {
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(p, 20.f));
+            positionInitialeDefinie = true;
+        }
+    }
+
+    private void getLastLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION }, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+
+                    latitude.setText(String.valueOf(location.getLatitude()));
+                    longitude.setText(String.valueOf(location.getLongitude()));
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+                    if (mapFragment != null)
+                        mapFragment.getMapAsync(MapActivity.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults,
+            int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
