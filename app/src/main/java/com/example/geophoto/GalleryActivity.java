6e7aff34508049a5ac2f61b6d4939d94f4180cd1
lam-vincent <mrccniv@gmail.com>
2025-05-14 13:05:10 +0200

    Fix photo selection functionality in GalleryActivity and update variable names

--- app/src/main/java/com/example/geophoto/GalleryActivity.java
+++ app/src/main/java/com/example/geophoto/GalleryActivity.java
@@ -1,20 +1,28 @@
 package com.example.geophoto;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.core.widget.ImageView;
-import androidx.core.widget.Button;
+
+import java.io.FileNotFoundException;
 
 public class GalleryActivity extends AppCompatActivity {
 
     private ImageView imgPhoto;
-    private Button btnPhoto;
+    private Button btnFindPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +37,12 @@ public class GalleryActivity extends AppCompatActivity {
     }
 
     private void initActivity() {
-        imgFindPhoto = (ImageView) findViewById(R.id.imgPhoto);
-        btnPhoto = (Button) findViewById(R.id.btnPhoto);
+        ImageView imgPhoto = (ImageView) findViewById(R.id.imgPhoto);
+        btnFindPhoto = (Button) findViewById(R.id.btnFindPhoto);
     }
 
     private void createOnClickPhotoButton() {
-        btnPhoto.setOnClickListener(new Button.OnClickListener() {
+        btnFindPhoto.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Ouvrir la galerie
@@ -53,7 +61,13 @@ public class GalleryActivity extends AppCompatActivity {
             Uri selectedImage = data.getData();
             imgPhoto.setImageURI(selectedImage);
             // BitmapFactory pour obtenir un bitmap et l'afficher dans l'ImageView
-            Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage));
+            Bitmap bitmap = null;
+            try {
+                assert selectedImage != null;
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
             imgPhoto.setImageBitmap(bitmap);
         } else {
             Toast.makeText(this, "Aucune image sélectionnée", Toast.LENGTH_SHORT).show();
